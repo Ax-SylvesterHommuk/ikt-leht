e12f4bb488219a2748f6f244c182e186e3db836a
Ax-SylvesterHommuk <ax-sylvester.hommuk@voco.ee>
2024-01-29 20:15:46 +0200

    Add VOCO styled navbar

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,3 +1,14 @@
 <template>
-  <h1>Home</h1>
+  <Navbar/>
+  <h1>Hello World!</h1>
 </template>
+
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'BLACKHAWK', 'sans-serif'],
+      },
+      fontWeight: {
+        'thin': 100,
+        'extralight': 200,
+        'light': 300,
+        'normal': 400,
+        'medium': 500,
+        'semibold': 600,
+        'bold': 700,
+        'extrabold': 800,
+        'black': 900,
+      },
+    },
   },
   plugins: [],
 }
