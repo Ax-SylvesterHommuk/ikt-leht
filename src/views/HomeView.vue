e1c36977f9d47971052dd085b3878bc367fd90b8
pingoDeLingo <robinnoormets@gmail.com>
2024-01-29 23:21:14 +0200

    Added text, image to main page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
   <Navbar/>
-  <h1>Hello World!</h1>
+  <div class="w-full h-[651px] flex bg-[url('../assets/mainbg.png')] ">
+    <div class="text-2xl text-white font-bold pt-60 pl-60">
+      Tarkvaraarendus. Andmeanalüüs. Küberjulgeolek. <br><br>
+      Uuri põnevat maailma IT erialade ja võimaluste osas ning liigu edasi VOCO rütmis.
+    </div>
+  </div>
 </template>
 
 <script>
