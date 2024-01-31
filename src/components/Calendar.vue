4afae8c7e1fd8d03deaad310e83525d8d2b2451c
Ax-SylvesterHommuk <ax-sylvester.hommuk@voco.ee>
2024-01-31 09:26:21 +0200

    Refactor Calendar component layout and add event display

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -1,90 +1,91 @@
 <template>
+  <div class="calendar">
+    <!-- Buttons for navigation -->
     <div class="flex">
-
-        <button class="btn-primary"
-                @click="reset()">Today</button>
-        <button class="btn"
-                @click="shiftMonth(-1)">Previous</button>
-        <button class="btn"
-                @click="shiftMonth(1)">Next</button>
-        <span class="text-3xl">{{ viewDate.format('MMMM YYYY') }}</span>
-
+      <button class="btn-primary" @click="reset()">Today</button>
+      <button class="btn" @click="shiftMonth(-1)">Previous</button>
+      <button class="btn" @click="shiftMonth(1)">Next</button>
+      <span class="text-3xl">{{ viewDate.format('MMMM YYYY') }}</span>
     </div>
+
+    <!-- Week days -->
     <div class="grid grid-cols-7 gap-1">
-        <div v-for="d in weekDays"
-             class="text-center">
-            <div>{{ d }}</div>
-        </div>
+      <div v-for="day in weekDays" class="text-center">{{ day }}</div>
     </div>
-    <div class="grid grid-cols-7">
-        <div v-for="p in daystoPrepend"></div>
-        <div class="border border-slate-200 flex flex-col h-32"
-             v-for="d in units">
-            <div :class="[d.isToday() ? 'bg-red-300' : '']" class="text-center">{{ d.format('D') }}</div>
 
+    <!-- Calendar days -->
+    <div class="grid grid-cols-7">
+      <div v-for="emptySlot in daystoPrepend"></div>
+      <div class="border border-slate-200 flex flex-col h-32" v-for="date in units">
+        <div :class="[date.isToday() ? 'bg-red-300' : '']" class="text-center">{{ date.format('D') }}</div>
+        <div class="events">
+          <!-- Display events for this date -->
+          <div v-for="event in getEventsForDate(date)">
+            <p>{{ event.Pealkiri }}</p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
-<script setup lang='ts'>
-import { ISODate } from '$/common/types/Notebook';
-import { computed, ref } from 'vue';
-
+<script>
 import dayjs from 'dayjs';
-import isToday from 'dayjs/plugin/isToday'
+import isToday from 'dayjs/plugin/isToday';
+
 dayjs.extend(isToday);
 
-type Props = {
-    modelValue?: any
-    startDate?: ISODate
-    display?: 'month' | 'year' | 'week' | 'day'
-}
-const props = withDefaults(defineProps<Props>(), {
-    modelValue: () => null,
-    display: () => 'month',
-    startDate: () => '2022-12-05'
-});
-const emits = defineEmits(['update:modelValue']);
+export default {
+  data() {
+    return {
+      viewDate: dayjs(),
+      weekDays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+      events: []
+    };
+  },
+  computed: {
+    daystoPrepend() {
+      const startOfMonth = this.viewDate.startOf("month");
+      const startOfFirstWeek = startOfMonth.startOf("week");
+      const daysToFirstDay = startOfMonth.diff(startOfFirstWeek, "day");
+      return Array.from(new Array(daysToFirstDay).keys());
+    },
+    
+    units() {
+      const ranges = [];
+      const startOfRange = this.viewDate.startOf('month').add(-1, 'day');
+      const endOfRange = this.viewDate.endOf('month').add(-1, 'day');
+      let currentDate = startOfRange;
 
-const viewDate = ref(dayjs(props.startDate));
-
-const daystoPrepend = computed(() => {
-    const startOfMonth = viewDate.value.startOf("month");
-    const startOfFirstWeek = startOfMonth.startOf("week");
-    const daysToFirstDay = startOfMonth.diff(startOfFirstWeek, "day");
-    return Array.from(new Array(daysToFirstDay).keys());
-})
-
-const units = computed(() => {
-    let ranges = [];
-    let startOfRange = viewDate.value.startOf('month').add(-1,'day');
-    let endOfRange = viewDate.value.endOf('month').add(-1,'day');
-
-    let currentDate = startOfRange;
-
-    while (currentDate.isBefore(endOfRange) || currentDate.isSame(endOfRange)) {
+      while (currentDate.isBefore(endOfRange) || currentDate.isSame(endOfRange)) {
         currentDate = currentDate.add(1, 'day');
         ranges.push(currentDate);
+      }
+      return ranges;
     }
-    return ranges;
-})
+  },
+  created() {
+    fetch('https://hommukaxsylvester.ikt.khk.ee/api/kalender.php')
+      .then(response => response.json())
+      .then(data => {
+        this.events = data;
+      })
+      .catch(error => {
+        console.error('Error fetching events:', error);
+      });
+  },
+  methods: {
+    shiftMonth(amount) {
+      this.viewDate = this.viewDate.add(amount, 'month');
+    },
 
-const shiftMonth = function (amount: number) {
-    viewDate.value = viewDate.value.add(amount, 'month');
-}
-const reset = function () {
-    viewDate.value = dayjs();
-}
+    reset() {
+      this.viewDate = dayjs();
+    },
 
-
-const weekDays = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
-]
-
+    getEventsForDate(date) {
+      return this.events.filter(event => dayjs(event.Kuupaev).isSame(date, 'day'));
+    }
+  }
+};
 </script>
