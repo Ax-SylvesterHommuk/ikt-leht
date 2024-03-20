e1b7bef6f7fab7abcf62fa04405087df32aeee8d
Ax-SylvesterHommuk <ax-sylvester.hommuk@voco.ee>
2024-03-20 10:52:05 +0200

    Add mobile view to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="font-montserrat font-medium w-full bg-white py-3 p-[1.25rem]">
+  <!-- Navbar for desktop -->
+  <div class="hidden md:block font-montserrat font-medium w-full bg-white py-3 p-[1.25rem]">
     <div class="container mx-auto flex items-center justify-between">
       <div class="flex items-center space-x-2 ml-36">
         <FacebookIcon class="text-black hover:text-[#c8d1e0] cursor-pointer h-6 w-6"/>
@@ -13,18 +14,41 @@
       </div>
     </div>
   </div>
-  <div class="font-montserrat font-medium w-full bg-black py-5 p-[1.25rem]">
+  <div class="hidden md:block font-montserrat font-medium w-full bg-black py-5 p-[1.25rem]">
     <div class="container mx-auto flex items-center justify-between">
       <div class="flex items-center">
         <img src="../assets/voco_it_kool_logo.svg" alt="Logo" class="h-[75px] cursor-pointer ml-40" @click="this.$router.push('/');">
       </div>
       <div class="flex items-center space-x-4 mr-32 text-[0.875rem]">
         <a href="https://siseveeb.voco.ee/kutseope/oppetoo/konsultatsioonid" class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1">Konsultatsioonid</a>
-        <button class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1" @click="this.$router.push('/praktika');">Praktika</button>
+        <button class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1" @click="sendRoute('/praktika')">Praktika</button>
         <button class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1">Kutsemeistrivõistlused</button>
         <button class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1">Tunnustused</button>
-        <button class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1" @click="this.$router.push('/uritused');">Üritused</button>
+        <button class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1" @click="sendRoute('/uritused')">Üritused</button>
       </div>
+    </div>
+  </div>
+
+  <!-- Navbar for mobile -->
+  <div class="relative md:hidden font-montserrat font-medium w-full bg-black py-3 p-[1.25rem] z-50">
+    <div class="container mx-auto flex items-center justify-between">
+      <div class="flex items-center">
+        <img src="../assets/voco_it_kool_logo.svg" alt="Logo" class="h-[75px] cursor-pointer ml-2 md:ml-40" @click="this.$router.push('/');">
+      </div>
+      <!-- Burger menu for mobile -->
+      <button class="text-white hover:text-[#c8d1e0] ease-in-out duration-200 px-3 py-1 md:hidden" @click="toggleMenu">
+        <BurgerMenu />
+      </button>
+    </div>
+    
+    <!-- Mobile menu overlay -->
+    <div class="absolute inset-x-0 top-full bg-white w-full py-4 md:hidden" :class="{ 'block': isMenuOpen, 'hidden': !isMenuOpen }">
+      <!-- * Menus -->
+      <a href="https://siseveeb.voco.ee/kutseope/oppetoo/konsultatsioonid" class="block text-black text-center py-2">Konsultatsioonid</a>
+      <a href="#" class="block text-black text-center py-2" @click="sendRoute('/praktika')">Praktika</a>
+      <a href="#" class="block text-black text-center py-2">Kutsemeistrivõistlused</a>
+      <a href="#" class="block text-black text-center py-2">Tunnustused</a>
+      <a href="#" class="block text-black text-center py-2" @click="sendRoute('/uritused')">Üritused</a>
     </div>
   </div>
 </template>
@@ -33,12 +57,27 @@
 import FacebookIcon from '@/components/icons/Facebook.vue';
 import TiktokIcon from '@/components/icons/Tiktok.vue';
 import InstagramIcon from '@/components/icons/Instagram.vue';
+import BurgerMenu from '@/components/icons/BurgerMenu.vue';
 
 export default {
   components: {
     FacebookIcon,
     TiktokIcon,
-    InstagramIcon
+    InstagramIcon,
+    BurgerMenu
   },
+  data() {
+    return {
+      isMenuOpen: false
+    }
+  },
+  methods: {
+    toggleMenu() {
+      this.isMenuOpen = !this.isMenuOpen;
+    },
+    sendRoute(route) {
+      this.$router.push(route);
+    }
+  }
 }
 </script>
---
+++ src/components/icons/BurgerMenu.vue
@@ -0,0 +1,3 @@
+<template>
+    <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M2 6h20a1 1 0 010 2H2a1 1 0 010-2zm20 5H2a1 1 0 000 2h20a1 1 0 000-2zm0 5H2a1 1 0 000 2h20a1 1 0 000-2z" clip-rule="evenodd"/></svg>
+</template>
