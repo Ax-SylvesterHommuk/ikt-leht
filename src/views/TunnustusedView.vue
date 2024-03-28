9b7d8aaa1c26ca63c7667e456bdb3ac6150d5936
Robin <robin.noormets@voco.ee>
2024-03-28 13:51:30 +0200

    Changed "Item" into correct name

--- src/views/TunnustusedView.vue
+++ src/views/TunnustusedView.vue
@@ -1,36 +1,36 @@
 <template>
-    <Navbar />
-    <div class="bg-gray-100 font-montserrat min-h-screen py-8 flex">
-        <!-- Sidebar -->
-        <div class="w-1/4"></div>
-        <div>
-            <ul class="text-lg text-left">
-                <li>
-                    <a href="#" class="border border-gray-300 py-2 px-4 w-64 block">Item 1</a>
-                </li>
-                <li>
-                    <a href="#" class="border border-gray-300 py-2 px-4 w-64 block">Item 2</a>
-                </li>
-                <li>
-                    <a href="#" class="border border-gray-300 py-2 px-4 w-64 block">Item 3</a>
-                </li>
-                <li>
-                    <a href="#" class="border border-gray-300 py-2 px-4 w-64 block">Item 4</a>
-                </li>
-                <li>
-                    <a href="#" class="border border-gray-300 py-2 px-4 w-64 block">Item 5</a>
-                </li>
-            </ul>
-        </div>
-        <!-- Content -->
-        <div class="w-2/4">
-            <div class="max-w-4xl mx-auto">
-                <h2 class="text-3xl font-bold mb-4">Tunnustused</h2>
-            </div>
-        </div>
+  <Navbar />
+  <div class="bg-gray-100 font-montserrat min-h-screen py-8 flex">
+    <!-- Sidebar -->
+    <div class="w-1/4"></div>
+    <div>
+      <ul class="text-lg text-left">
+        <li>
+          <a href="#" class="border border-gray-300 py-2 px-4 w-64 block hover:bg-gray-200 hover:border-gray-400 transition duration-300 ease-in-out">Meie autahvel</a>
+        </li>
+        <li>
+          <a href="#" class="border border-gray-300 py-2 px-4 w-64 block hover:bg-gray-200 hover:border-gray-400 transition duration-300 ease-in-out">Meie Meister</a>
+        </li>
+        <li>
+          <a href="#" class="border border-gray-300 py-2 px-4 w-64 block hover:bg-gray-200 hover:border-gray-400 transition duration-300 ease-in-out">Aasta õpetajad</a>
+        </li>
+        <li>
+          <a href="#" class="border border-gray-300 py-2 px-4 w-64 block hover:bg-gray-200 hover:border-gray-400 transition duration-300 ease-in-out">Tänukirjad</a>
+        </li>
+        <li>
+          <a href="#" class="border border-gray-300 py-2 px-4 w-64 block hover:bg-gray-200 hover:border-gray-400 transition duration-300 ease-in-out">Õppijate saavutused</a>
+        </li>
+      </ul>
     </div>
+    <!-- Content -->
+    <div class="w-2/4">
+      <div class="max-w-4xl mx-auto">
+        <h2 class="text-3xl font-bold mb-4">Tunnustused</h2>
+      </div>
+    </div>
+  </div>
 
-    <Footer />
+  <Footer />
 </template>
 
 <script>
